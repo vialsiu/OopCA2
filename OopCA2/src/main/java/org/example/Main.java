e3eaa386d1912f63dc7ee25e63a3bd1e5c3665fe
Vialsiu <D00265675@student.dkit.ie>
2024-11-27 12:24:50 +0000

    test commit

--- OopCA2/src/main/java/org/example/Main.java
+++ OopCA2/src/main/java/org/example/Main.java
@@ -12,8 +12,18 @@ public class Main {
 
         while (true) {
             System.out.println("\nMenu:");
-            System.out.println("3. Question 3");
-            System.out.println("2. Exit");
+            System.out.println("1. Question 1 - Goods Containers");
+            System.out.println("2. Question 2 - Car Parking");
+            System.out.println("3. Question 3 - Nested HTML Tags");
+            System.out.println("4. Question 4 - Flood Fill");
+            System.out.println("5. Question 5 - Java Identifier Count");
+            System.out.println("6. Question 6 - Airport Flights");
+            System.out.println("7. Question 7 - Stocks Shares Tax");
+            System.out.println("8. Question 8 - Multi Company Stocks");
+            System.out.println("9. Question 9 - Arithmetic Expression Calculator");
+            System.out.println("10. Question 10 - Backtracking through a Maze");
+            System.out.println("11. Question 11 - Shortest Distance to City");
+            System.out.println("12. Exit");
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -23,6 +33,10 @@ public class Main {
                     System.out.print("Enter the file name containing HTML tags: ");
                     String fileName = scanner.nextLine();
                     question3.processFile(fileName);
+                    break;
+
+                case 4:
+                    System.out.print("Flood Fill Problem: ");
                     break;
 
                 case 2:
@@ -67,6 +81,7 @@ public class Main {
                         System.out.println("The tags are properly nested.");
                     } else {
                         System.out.println("The tags are NOT properly nested.");
+                        System.out.println("new");
                     }
                 }
             } catch (IOException e) {
